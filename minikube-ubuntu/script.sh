7b8d34f033c2f0b6f05b4f666efe26436d5a8b2e
fernandosalesti <fernandosalesti@gmail.com>
2024-01-15 19:19:25 -0300

    ADD minikube

---
+++ minikube-ubuntu/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x ./minikube
+sudo mv ./minikube /usr/local/bin/minikube
+alias kubectl="minikube kubectl --"
+
+# Alguns comandos
+#minikube version
+#minikube start --driver=docker
+#minikube start --driver=docker -p prod --nodes=2
+#minikube delete --all
