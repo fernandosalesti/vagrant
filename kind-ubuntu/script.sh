bd2a3dd6090ebd51e3e4774906dd2d78c52745e6
fernandosalesti <fernandosalesti@gmail.com>
2024-02-01 18:42:24 -0300

    add kubectl completion

--- kind-ubuntu/script.sh
+++ kind-ubuntu/script.sh
@@ -15,6 +15,9 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update && sudo apt install -y kubectl
 
+echo 'source <(kubectl completion bash)' >> .bashrc
+source .bashrc
+
 # Alguns comandos
 
 # systemctl status docker
--- minikube-ubuntu/script.sh
+++ minikube-ubuntu/script.sh
@@ -14,8 +14,8 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update && sudo apt install -y kubectl
 
-# sudo echo 'alias kubectl="minikube kubectl --"' >> .bashrc
-# sudo .bashrc
+echo 'source <(kubectl completion bash)' >> .bashrc
+source .bashrc
 # Alguns comandos
 # minikube version
 # minikube start --driver=docker
